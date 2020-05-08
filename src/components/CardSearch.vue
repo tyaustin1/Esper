68b4c8bb85e83ad68a589385c1fca2f49b5089a5
Ty Austin <tyaustin@Tys-MacBook-Pro.local>
2020-05-08 16:32:58 -0400

    Create card search component and api call
    Create card search component with text box that makes call to eidolon for image and displays it

---
+++ src/components/CardSearch.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id='card-search'>
+    <input type="text" v-model=cardName>
+    <button @click=getCard(cardName)>Make Call</button>
+    <p>
+      <img :src=imageUrl>
+    </p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'CardSearch',
+
+  data () {
+    return {
+      cardName: '',
+      imageUrl: ''
+    };
+  },
+
+  methods: {
+    getCard (cardName) {
+      axios.get('http://localhost:8080/card/' + cardName).then(response => {
+      this.card = response;
+      this.imageUrl = this.card.data.imageUrl;
+      });
+    }
+  }
+  
+}
+</script>
+<style scoped>
+</style>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+'use strict'
+// Template version: {{ template_version }}
+// see http://vuejs-templates.github.io/webpack for documentation.
+
+const path = require('path')
+
+module.exports = {
+  devServer: {
+    // Various Dev Server settings
+    host: 'localhost', // can be overwritten by process.env.HOST
+    port: 8085, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+
+  },
+}
